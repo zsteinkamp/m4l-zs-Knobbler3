21e5afe684fa5d6936b8dd446589e3027c9a4d2e
Zack Steinkamp <zack@steinkamp.us>
2022-10-10 18:24:50 -0700

    fix toggleInput

--- toggleInput.js
+++ toggleInput.js
@@ -3,7 +3,8 @@ outlets=1;
 
 var origInputs = {};
 
-var lo;
+var lo = null;
+var currTrack = null;
 var debugLog = false;
 
 function debug() {
@@ -14,25 +15,30 @@ function debug() {
 
 debug("reloaded");
 
-function getTrackStatus(track) {
+function getTrackStatus() {
   var airt = null;
   var currentInput = null;
   var noInput = null;
   var allInputs = null;
   var inputEnabled = false;
-  if (track.get("can_be_armed") == "1") {
-    var airt = JSON.parse(track.get('available_input_routing_types')).available_input_routing_types;
-    currentInput = JSON.parse(track.get('input_routing_type')).input_routing_type;
+  debug(currTrack.type);
+  if (currTrack.get("is_foldable") == "0" && currTrack.get("can_be_frozen") == "1") {
+    debug("IN HERE");
+    var airt = JSON.parse(currTrack.get('available_input_routing_types')).available_input_routing_types;
+    currentInput = JSON.parse(currTrack.get('input_routing_type')).input_routing_type;
     allInputs = airt[0];
     noInput = airt[airt.length - 1]; // "No Input" is the last available input routing type
     inputEnabled = currentInput.display_name !== noInput.display_name;
   }
 
-  return { currentInput: currentInput, noInput: noInput, inputEnabled: inputEnabled, allInputs: allInputs };
+  var ret = { currentInput: currentInput, noInput: noInput, inputEnabled: inputEnabled, allInputs: allInputs }
+  debug(JSON.stringify(ret));
+  return ret;
 }
 
-function updateTrackDisplay(track) {
-  var trackStatus = getTrackStatus(track);
+function updateTrackDisplay() {
+  var trackStatus = getTrackStatus();
+  debug('inputEnabled?', trackStatus.inputEnabled);
   if (trackStatus.inputEnabled) {
     outlet(0, ['/toggleInput', 1]);
   } else {
@@ -43,39 +49,39 @@ function updateTrackDisplay(track) {
 function currentTrackCallback(a) {
   var args = arrayfromargs(a);
   if (args.shift() !== 'selected_track') {
-    //post("RETURNING1\n");
+    debug("RETURNING1");
     return;
   }
   var trackId = args.join(" ");
   if (trackId === 'id 0') {
-    //post("RETURNING2\n");
+    debug("RETURNING2");
     return;
   }
-  //post("ARGS3: " + trackId + "\n");
-  var track = new LiveAPI(trackId);
-  updateTrackDisplay(track);
+  currTrack = new LiveAPI(trackId);
+  updateTrackDisplay();
 }
 
 function init() {
   //post("INIT\n");
   lo = new LiveAPI(currentTrackCallback, 'live_set view');
+  lo.mode = 1;
   lo.property = 'selected_track';
 }
 
 function toggle() {
-  var liveObject = new LiveAPI('live_set view selected_track');
-  var trackStatus = getTrackStatus(liveObject);
+  debug("IN TOGGLE");
+  var trackStatus = getTrackStatus();
   if (trackStatus.inputEnabled) {
-    origInputs[liveObject.id] = trackStatus.currentInput;
+    origInputs[currTrack.id] = trackStatus.currentInput;
     // set to No Input
     ret = trackStatus.noInput;
   } else {
     // set to Original, TODO default to All Inputs
-    ret = origInputs[liveObject.id] || trackStatus.allInputs;
+    ret = origInputs[currTrack.id] || trackStatus.allInputs;
   }
 
   if (trackStatus.currentInput) {
-    liveObject.set('input_routing_type', ret);
+    currTrack.set('input_routing_type', ret);
   }
-  updateTrackDisplay(liveObject);
+  updateTrackDisplay();
 }
