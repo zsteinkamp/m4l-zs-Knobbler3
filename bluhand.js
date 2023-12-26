19025976c079d269757dcf6c9f26e1545a96e9c3
Zack Steinkamp <zack@steinkamp.us>
2023-12-26 10:40:13 -0800

    add bank controls to bluhand (thanks @mrJonesmusic)

--- bluhand.js
+++ bluhand.js
@@ -2,21 +2,23 @@
 // M4L Config
 ////////////////////////////////////////////////
 
-autowatch = 1;
-outlets = 1;
+autowatch = 1
+outlets = 1
 
-var OUTLET_OSC = 0;
-var MAX_PARAMS = 33; // "Device On" is always the first
+var OUTLET_OSC = 0
+var PAGE_SIZE = 16 // "Device On" is always the first
 
-setoutletassist(OUTLET_OSC, "OSC Messages");
+setoutletassist(OUTLET_OSC, 'OSC Messages')
 
 ////////////////////////////////////////////////
 // VARIABLES
 ////////////////////////////////////////////////
 
-var debugLog = false;
+var debugLog = false
 
 var data = {
+  currBank: 0,
+  paramIdArr: [],
   trackName: null,
   deviceName: null,
   params: [],
@@ -26,18 +28,18 @@ var data = {
     params: null,
   },
   objIdToParamIdx: {},
-};
-var lomParamsArr = [];
-var nullString = "- - -";
+}
+var lomParamsArr = []
+var nullString = '- - -'
 
-debug("reloaded");
+debug('reloaded')
 
 ////////////////////////////////////////////////
 // EXTERNAL METHODS
 ////////////////////////////////////////////////
 
 function bang() {
-  setupListener();
+  setupListener()
 }
 
 ////////////////////////////////////////////////
@@ -45,181 +47,251 @@ function bang() {
 ////////////////////////////////////////////////
 
 function setupListener() {
-  debug("SETUP LISTENERS");
+  debug('SETUP LISTENERS')
 
-  data.observers.trackName = new LiveAPI(trackNameCallback, "live_set view selected_track");
-  data.observers.trackName.mode = 1;
-  data.observers.trackName.property = "name";
+  data.observers.trackName = new LiveAPI(
+    trackNameCallback,
+    'live_set view selected_track'
+  )
+  data.observers.trackName.mode = 1
+  data.observers.trackName.property = 'name'
 
-  data.observers.deviceName = new LiveAPI(deviceNameCallback, "live_set appointed_device");
-  data.observers.deviceName.mode = 1;
-  data.observers.deviceName.property = "name";
+  data.observers.deviceName = new LiveAPI(
+    deviceNameCallback,
+    'live_set appointed_device'
+  )
+  data.observers.deviceName.mode = 1
+  data.observers.deviceName.property = 'name'
 
-  data.observers.params = new LiveAPI(parametersCallback, "live_set appointed_device");
-  data.observers.params.mode = 1;
-  data.observers.params.property = "parameters";
+  data.observers.params = new LiveAPI(
+    parametersCallback,
+    'live_set appointed_device'
+  )
+  data.observers.params.mode = 1
+  data.observers.params.property = 'parameters'
 }
 
 function colorToString(colorVal) {
-  var retString = parseInt(colorVal).toString(16).toUpperCase();
-  var strlen = retString.length;
+  var retString = parseInt(colorVal).toString(16).toUpperCase()
+  var strlen = retString.length
   for (var i = 0; i < 6 - strlen; i++) {
-    retString = "0" + retString;
+    retString = '0' + retString
   }
-  return retString + 'FF';
+  return retString + 'FF'
 }
 
 function trackColorCallback(args) {
-  debug("TRACKCOLOR", args);
-  var args = arrayfromargs(args);
-  if (args[0] === "color") {
-    param.trackColor = colorToString(args[1]);
-    sendColor();
+  debug('TRACKCOLOR', args)
+  var args = arrayfromargs(args)
+  if (args[0] === 'color') {
+    param.trackColor = colorToString(args[1])
+    sendColor()
   }
 }
 
 function trackNameCallback(args) {
-  debug('TRACK ID', parseInt(this.id));
-  debug(args);
+  debug('TRACK ID', parseInt(this.id))
+  debug(args)
   if (parseInt(this.id) === 0) {
-    data.trackName = "none";
+    data.trackName = 'none'
   } else {
-    data.trackName = this.get("name");
+    data.trackName = this.get('name')
   }
-  data.trackColor = colorToString(this.get("color"));
-  debug('TRACKCOLOR', data.trackColor);
-  updateDeviceName();
+  data.trackColor = colorToString(this.get('color'))
+  debug('TRACKCOLOR', data.trackColor)
+  updateDeviceName()
 }
+
 function deviceNameCallback(args) {
-  debug('DEVICE ID', parseInt(this.id));
+  debug('DEVICE ID', parseInt(this.id))
   if (parseInt(this.id) === 0) {
-    data.deviceName = "none";
+    data.deviceName = 'none'
+  } else {
+    data.deviceName = this.get('name')
   }
-  else {
-    data.deviceName = this.get("name");
-  }
-  updateDeviceName();
+  updateDeviceName()
 }
 
 function updateDeviceName() {
-  var message = ["/bcurrDeviceName", data.trackName + " > " + data.deviceName];
+  var message = ['/bcurrDeviceName', data.trackName + ' > ' + data.deviceName]
   if (!(data.trackName && data.deviceName)) {
-    message = ["/bcurrDeviceName", "No device selected"];
+    message = ['/bcurrDeviceName', 'No device selected']
   }
-  debug(message);
-  outlet(OUTLET_OSC, message);
+  debug(message)
+  outlet(OUTLET_OSC, message)
 }
 
 function paramKey(paramObj) {
-  var key = paramObj.id.toString();
-  debug(key);
-  return key;
+  var key = paramObj.id.toString()
+  debug(key)
+  return key
 }
 
 function parametersCallback(args) {
-  debug(JSON.stringify(args));
-  paramIdArr = this.get("parameters");
+  debug(JSON.stringify(args))
+  if (parseInt(this.id) === 0) {
+    return
+  }
+  data.paramIdArr = this.get('parameters').filter(function (p) {
+    return p !== 'id'
+  })
+  data.currBank = 0
 
-  data.params = [];
-  data.objIdToParamIdx = {};
+  //debugLog = true
+  debug(data.paramIdArr.join(','))
+  //debugLog = false
 
-  var message;
-  var paramIdArrElem;
-  var currParam;
-  var paramIdx;
+  refreshParams()
+}
 
-  if (parseInt(this.id) !== 0) {
-    while (paramIdArr.length > 0 && data.params.length < MAX_PARAMS) {
-      paramIdArrElem = paramIdArr.shift();
-      if (paramIdArrElem === 'id') {
-        continue;
-      }
+function refreshParams() {
+  data.params = []
+  data.objIdToParamIdx = {}
 
-      paramIdx = data.params.length;
+  var message
+  var paramIdArrElem
+  var currParam
+  var paramIdx
 
-      currParam = {
-        paramObj: new LiveAPI(valueCallback, "id " + paramIdArrElem)
-      };
-      data.objIdToParamIdx[paramKey(currParam.paramObj)] = paramIdx;
-      currParam.name = currParam.paramObj.get("name").toString();
-      currParam.val = parseFloat(currParam.paramObj.get("value"));
-      currParam.min = parseFloat(currParam.paramObj.get("min")) || 0;
-      currParam.max = parseFloat(currParam.paramObj.get("max")) || 1;
+  var paramIdxVec = [data.paramIdArr[0]]
+  for (
+    var i = data.currBank * PAGE_SIZE + 1;
+    i < data.paramIdArr.length && paramIdxVec.length <= PAGE_SIZE + 1;
+    i++
+  ) {
+    paramIdxVec.push(data.paramIdArr[i])
+  }
 
-      message = ["/bparam" + paramIdx, currParam.name];
-      outlet(OUTLET_OSC, message);
+  for (var i = 0; i < paramIdxVec.length; i++) {
+    paramIdArrElem = paramIdxVec[i]
 
-      data.params.push(currParam);
-      data.params[paramIdx].paramObj.property = "value";
+    paramIdx = data.params.length
 
-      sendVal(paramIdx);
+    currParam = {
+      paramObj: new LiveAPI(valueCallback, 'id ' + paramIdArrElem),
     }
+    data.objIdToParamIdx[paramKey(currParam.paramObj)] = paramIdx
+    currParam.name = currParam.paramObj.get('name').toString()
+    ;(currParam.val = parseFloat(currParam.paramObj.get('value'))),
+      (currParam.min = parseFloat(currParam.paramObj.get('min')) || 0),
+      (currParam.max = parseFloat(currParam.paramObj.get('max')) || 1),
+      (message = ['/bparam' + paramIdx, currParam.name])
+    outlet(OUTLET_OSC, message)
+
+    data.params.push(currParam)
+    data.params[paramIdx].paramObj.property = 'value'
+
+    sendVal(paramIdx)
   }
 
   // zero-out the rest of the param sliders
-  for (paramIdx = data.params.length; paramIdx < MAX_PARAMS; paramIdx++) {
-    outlet(OUTLET_OSC, ["/bparam" + paramIdx, nullString]);
-    outlet(OUTLET_OSC, ["/bvalStr" + paramIdx, nullString]);
-    outlet(OUTLET_OSC, ["/bval" + paramIdx, 0]);
-    outlet(OUTLET_OSC, ["/bval" + paramIdx + "color", "FF000099"]);
+  for (paramIdx = data.params.length; paramIdx < PAGE_SIZE + 1; paramIdx++) {
+    outlet(OUTLET_OSC, ['/bparam' + paramIdx, nullString])
+    outlet(OUTLET_OSC, ['/bval' + paramIdx, 0])
+    outlet(OUTLET_OSC, ['/bval' + paramIdx + 'color', 'FF000099'])
   }
+
+  // update the current bank string
+  message = ['/bTxtCurrBank', 'Bank ' + (data.currBank + 1)]
+  debug(message)
+  outlet(OUTLET_OSC, message)
 }
 
 function sendVal(paramIdx) {
-  if (typeof(paramIdx) !== "number" || paramIdx < 0 || paramIdx >= MAX_PARAMS) { return; }
+  if (
+    typeof paramIdx !== 'number' ||
+    paramIdx < 0 ||
+    paramIdx >= PAGE_SIZE + 1
+  ) {
+    return
+  }
 
-  var param = data.params[paramIdx];
+  var param = data.params[paramIdx]
 
   // the value, expressed as a proportion between the param min and max
-  var outVal = (param.val - param.min) / (param.max - param.min);
+  var outVal = (param.val - param.min) / (param.max - param.min)
 
-  var message = ["/bval" + paramIdx, outVal]
-  debug(message);
-  outlet(OUTLET_OSC, message);
-  outlet(OUTLET_OSC, ["/bval" + paramIdx + "color", data.trackColor]);
-  outlet(OUTLET_OSC, ["/bvalStr" + paramIdx, param.paramObj.call("str_for_value", outVal)]);
+  var message = ['/bval' + paramIdx, outVal]
+  debug(message)
+  outlet(OUTLET_OSC, message)
+  outlet(OUTLET_OSC, ['/bval' + paramIdx + 'color', data.trackColor])
 }
 
 function valueCallback(args) {
-  var argsArr = arrayfromargs(args);
-  if (argsArr[0] !== "value") {
-    return;
+  var argsArr = arrayfromargs(args)
+  if (argsArr[0] !== 'value') {
+    return
   }
 
-  debug("TOPARGS", argsArr);
-  var paramIdx = data.objIdToParamIdx[paramKey(this)];
+  debug('TOPARGS', argsArr)
+  var paramIdx = data.objIdToParamIdx[paramKey(this)]
   if (paramIdx === undefined) {
-    debug("no data.objIdToParamIdx for", paramIdx, JSON.stringify(data.objIdToParamIdx));
-    return;
+    debug(
+      'no data.objIdToParamIdx for',
+      paramIdx,
+      JSON.stringify(data.objIdToParamIdx)
+    )
+    return
   }
   if (!data.params[paramIdx]) {
-    debug("no data.params for", paramIdx, JSON.stringify(data.params));
-    return;
+    debug('no data.params for', paramIdx, JSON.stringify(data.params))
+    return
   }
 
   // ensure the value is indeed changed (vs a feedback loop)
   if (argsArr[1] === data.params[paramIdx].val) {
-    debug(paramIdx, paramIdx.val, "NO CHANGE");
-    return;
+    debug(paramIdx, paramIdx.val, 'NO CHANGE')
+    return
   }
-  data.params[paramIdx].val = argsArr[1];
-  sendVal(paramIdx);
+  data.params[paramIdx].val = argsArr[1]
+  sendVal(paramIdx)
+}
+
+function receiveVal(matches) {
+  var paramIdx = parseInt(matches[1])
+  var param = data.params[paramIdx]
+  if (param) {
+    var value = param.min + parseFloat(matches[2]) * (param.max - param.min)
+    param.paramObj.set('value', value)
+  }
+}
+
+function receiveBank(matches) {
+  //debugLog = true
+  debug(matches)
+  if (data.paramIdArr.length === 0) {
+    return
+  }
+  var maxBank = Math.floor(data.paramIdArr.length / PAGE_SIZE)
+  debug(data.paramIdArr.length, PAGE_SIZE, maxBank)
+  if (matches[1] === 'Next') {
+    debug('NextBank')
+    if (data.currBank < maxBank) {
+      data.currBank += 1
+      refreshParams()
+    }
+  } else {
+    debug('PrevBank')
+    if (data.currBank > 0) {
+      data.currBank -= 1
+      refreshParams()
+    }
+  }
+  //debugLog = false
 }
 
 function oscReceive(args) {
-  var matches = args.match(/^\/bval(\d+) ([0-9.-]+)$/);
-  debug(JSON.stringify(matches));
-
-  if (!matches || matches.length !== 3) {
-    return;
-  }
-
-  var paramIdx = parseInt(matches[1]);
-  var param = data.params[paramIdx];
-  if (param) {
-    var value = param.min + (parseFloat(matches[2]) * (param.max - param.min));
-    param.paramObj.set("value", value);
-    outlet(OUTLET_OSC, ["/bvalStr" + paramIdx, param.paramObj.call("str_for_value", value)]);
+  debug(args)
+  var matchers = [
+    { regex: /^\/bval(\d+) ([0-9.-]+)$/, fn: receiveVal },
+    { regex: /^\/bbank(Prev|Next)$/, fn: receiveBank },
+  ]
+  for (var i = 0; i < matchers.length; i++) {
+    var matches = args.match(matchers[i].regex)
+    debug(JSON.stringify(matches))
+    if (matches) {
+      return matchers[i].fn(matches)
+    }
   }
 }
 
@@ -229,10 +301,14 @@ function oscReceive(args) {
 
 function debug() {
   if (debugLog) {
-    post(debug.caller ? debug.caller.name : 'ROOT', Array.prototype.slice.call(arguments).join(" "), "\n");
+    post(
+      debug.caller ? debug.caller.name : 'ROOT',
+      Array.prototype.slice.call(arguments).join(' '),
+      '\n'
+    )
   }
 }
 
 function dequote(str) {
-  return str.replace(/^"|"$/g, '');
+  return str.replace(/^"|"$/g, '')
 }
